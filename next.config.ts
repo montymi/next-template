f3428f9dc1768d79c226abe9ead50f8998397f34
Michael Montanaro <mcmontanaro01@gmail.com>
2025-01-25 23:55:16 -0500

    feat: add next.config.js with experimental Turbo and SVG loading using SVGR Webpack

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  // disable: process.env.NODE_ENV === "development",
+});
+
+// next.js config
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
+  reactStrictMode: false,
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
