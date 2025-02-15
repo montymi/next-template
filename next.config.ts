a9b97f3cb32044b2671e55da0ee302d67e6d482e
Michael Montanaro <mcmontanaro01@gmail.com>
2025-02-15 15:09:39 -0500

    feat(next.config): add dynamic locale support loaded from new script

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,15 @@
 import type { NextConfig } from 'next'
+import locales from './src/locales.json'
+
+// next.js config
+const nextConfig: NextConfig = {
+	reactStrictMode: true,
+	i18n: {
+		locales,
+		defaultLocale: 'en',
+		localeDetection: false,
+	},
+}
 
 const withPWA = require('next-pwa')({
 	dest: 'public',
@@ -7,14 +18,4 @@ const withPWA = require('next-pwa')({
 	disable: process.env.NODE_ENV === 'development',
 })
 
-// next.js config
-const nextConfig: NextConfig = {
-	reactStrictMode: true,
-	i18n: {
-		locales: ['en', 'de'],
-		defaultLocale: 'en',
-		localeDetection: false,
-	},
-}
-
-module.exports = withPWA(nextConfig)
+export default withPWA(nextConfig)
