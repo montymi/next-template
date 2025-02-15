9621648ca012c3198d5b81046ef1ef63f8445000
Michael Montanaro <mcmontanaro01@gmail.com>
2025-02-15 15:07:13 -0500

    feat(scripts): add prebuild script for finding new languages in public/locales

---
+++ scripts/generate-locale-map.js
@@ -0,0 +1,20 @@
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const localeDir = path.join(__dirname, '..', 'public', 'locales')
+const targetFile = path.join(__dirname, '..', 'src', 'locales.json')
+
+const generateLocaleMap = () => {
+	const folders = fs.readdirSync(localeDir).filter((item) => {
+		const itemPath = path.join(localeDir, item)
+		return fs.statSync(itemPath).isDirectory()
+	})
+	fs.writeFileSync(targetFile, JSON.stringify(folders, null, 2))
+	console.log('locales.json created.')
+}
+
+generateLocaleMap()
