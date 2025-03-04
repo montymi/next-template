a6b543ffae4e0283c5b765eae76683547d86bcb2
Michael Montanaro <mcmontanaro01@gmail.com>
2025-03-03 23:22:26 -0500

    feat(_document.test): add basic app renders without crashing test for custom next _document page

---
+++ __tests__/pages/_document.test.tsx
@@ -0,0 +1,27 @@
+import { render } from '@testing-library/react';
+
+jest.mock('next/document', () => ({
+    __esModule: true,
+    Html: () => null,
+    Head: () => null,
+    Main: () => null,
+    NextScript: () => null,
+    default: class extends require('react').Component {
+        static async getInitialProps() {
+            return { head: [], html: '', styles: [] };
+        }
+        render() {
+            return <div>Mocked Document</div>;
+        }
+    },
+}));
+
+import Document from '@/pages/_document';
+
+describe('Document', () => {
+    it('renders without crashing', () => {
+        expect(() => {
+            render(<Document />);
+        }).not.toThrow();
+    });
+});
